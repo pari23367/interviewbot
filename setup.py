e9e9c38837dd5ca9593b00354113d72c783bde3c
pari23367 <paridhi23367@iiitd.ac.in>
2025-05-27 17:53:51 +0530

    added proj struc

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'interview Chatbot',
+    version= '0.0.0',
+    author= 'paridhi',
+    author_email= 'kotaryaparidhi19@gmail.com',
+    
+    packages= find_packages(),
+    install_requires = []
+
+)
